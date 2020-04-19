69ef43ee650cc0d729615ed004884386c1cab048
Kasper Henriksen <schonsteren@gmail.com>
2020-04-19 15:54:23 +0200

    Updated folders and scripts.

--- Preprocessing/SubSampling/pointcloud_subsampling.bat
+++ Preprocessing/SubSampling/pointcloud_subsampling.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 set /p PATH_TO_CLOUDCOMPARE= Enter path to Cloud Compare: 
-cd /D "C:\Program Files\CloudCompare"
+cd /D %PATH_TO_CLOUDCOMPARE%
 
 set /p PATH_TO_POINTCLOUDS= Enter path to point clouds: 
 
@@ -9,7 +9,7 @@ for /d %%i in (%PATH_TO_POINTCLOUDS%\*) do ( ::Goes through each directory in gi
 	for %%f in (%%i\*) do ( ::Goes through each file in the directory
 		set /p val=<%%f
   		echo "path: %%f"
-		CloudCompare -SILENT -O %%f -C_EXPORT_FMT ASC -EXT CSV -PREC 6 -SS OCTREE 8 ::SubSampling of Octree
+		CloudCompare -SILENT -O %%f -C_EXPORT_FMT ASC -EXT CSV -PREC 6 -SEP COMMA -SS OCTREE 8 ::SubSampling of Octree
 		del %%f
 	)
 )
