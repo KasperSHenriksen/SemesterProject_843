79d17f1f2a8a32366f00d287eebc3825901b5325
Lynzzen <mlynge16@student.aau.dk>
2020-04-24 13:39:11 +0200

    Final update for testing

--- ComputerVision_miniproject/main.py
+++ ComputerVision_miniproject/main.py
@@ -31,17 +31,21 @@ device = torch.device("cuda")
 model = DGCNN(numClass = NUM_CLASS, emb_dims = EMB_DIMS, dropout_rate=DROPOUT_RATE, batch_size = TRAIN_BATCH_SIZE, k = K).to(device)
 
 def train():
+    #Loading the dataloaders for train and test
     train_loader = DataLoader(ModelNet40(partition='train', num_points=NUM_POINTS), num_workers=8,
                               batch_size=TRAIN_BATCH_SIZE, shuffle=True, drop_last=True)
     test_loader = DataLoader(ModelNet40(partition='test', num_points=NUM_POINTS), num_workers=8,
                              batch_size=TEST_BATCH_SIZE, shuffle=True, drop_last=False)
 
+    #Optimizer
     optimizer = optim.SGD(model.parameters(),lr = LR*100 , momentum = 0.9, weight_decay = 1e-4)
-
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, EPOCHS, eta_min= LR)
 
-    criterion = cal_loss
+    #Loss function
+    criterion = cal_loss #FOR US: Maybe just overwrite this with a function instead of calling into utils, as we just use one line from the other script. 
     #criterion = F.cross_entropy
+
+    #Lists to save information such as accuracy and loss for each epoch.
     best_test_accuracy = 0
     
     train_epoch_accuracy_normal = []
@@ -53,6 +57,7 @@ def train():
     test_epoch_loss = []
 
     for epoch in range(EPOCHS):
+        #Training starts here
         print('[INFO] Training...')        
         scheduler.step()
 
@@ -84,8 +89,7 @@ def train():
             train_loss += loss.item()*batch_size
             train_true.append(label.cpu().numpy())
             train_pred.append(predictions.detach().cpu().numpy())
-            
-        
+
         train_pred = np.concatenate(train_pred)
         train_true = np.concatenate(train_true)
 
@@ -103,7 +107,9 @@ def train():
         np.savetxt('Train_Accuracy_average.csv',np.array(train_epoch_accuracy_average))
         np.savetxt('Train_Loss.csv',np.array(train_epoch_loss))
 
-        #Test
+        #Training stops here
+
+        #Testing starts here
         test_loss = 0.0
         count = 0.0
         print("Testing starts ln45")
@@ -128,7 +134,6 @@ def train():
         test_pred = np.concatenate(test_pred)
         test_true = np.concatenate(test_true)
 
-        #print(f'Test {epoch}| Loss: {test_loss*1.0/count}| Test acc: {metrics.accuracy_score(test_true,test_pred)}| Test avg acc: {metrics.balanced_accuracy_score(test_true,test_pred)}')
         #Save accuracy and loss to a list
         normal_accuracy = metrics.accuracy_score(test_true,test_pred)
         average_accuracy = metrics.balanced_accuracy_score(test_true,test_pred)
@@ -143,11 +148,16 @@ def train():
         np.savetxt('Test_Accuracy_average.csv',np.array(test_epoch_accuracy_average))
         np.savetxt('Test_Loss.csv',np.array(test_epoch_loss))
 
+        #Testing stops here
+
+        #The model with the highest found accuracy for testing is saved
         if normal_accuracy > best_test_accuracy:
             best_test_accuracy = normal_accuracy 
             print('[INFO] Saving Model...')
             np.savetxt('Epoch.txt',np.array([epoch]))
             torch.save(model.state_dict(),'model.t7')
+
+        
 
 
 def test():
@@ -188,7 +198,120 @@ def test():
 
 
 
+
+
+
+
+def train1(model, mode, dataloader, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
+    print('[INFO] {mode}...')
+    #Selecting which mode to run the model (Training or Testing)
+    if mode == 'Training':        
+        scheduler.step()
+        model.train()
+    else:
+        model.eval()
+
+    loss_sum = 0.0
+    count = 0.0
+    predicted_labels = []
+    correct_labels = []
+    
+    for data, label in tqdm(dataloader):
+        data = data.to(device) 
+        label = label.to(device).squeeze()
+
+        data = data.permute(0,2,1)
+        batch_size = data.size()[0]
+        
+        if mode == 'Training':
+            optimizer.zero_grad() #zero_grad clears old gradients from the last step (otherwise you’d just accumulate the gradients from all loss.backward() calls).
+
+        output = model(data) #Calculates class probabilities for each point cloud. So if batchsize is 16 and num classes is 40, it returns [16,40].
+        loss = criterion(output,label,False) 
+
+        if mode == 'Training':
+            loss.backward() #computes the derivative of the loss w.r.t. the parameters (or anything requiring gradients) using backpropagation.
+            optimizer.step() #causes the optimizer to take a step based on the gradients of the parameters.
+
+        predictions = output.max(dim=1)[1]
+        count += batch_size
+
+        loss_sum += loss.item()*batch_size
+        correct_labels.append(label.cpu().numpy())
+        predicted_labels.append(predictions.detach().cpu().numpy()) 
+
+    #Concats 
+    predicted_labels = np.concatenate(predicted_labels)
+    correct_labels = np.concatenate(correct_labels)
+
+    #Computes accuracy and loss based on metrics
+    normal_accuracy = metrics.accuracy_score(correct_labels,predicted_labels)
+    average_accuracy = metrics.balanced_accuracy_score(correct_labels,predicted_labels)
+    loss_sum = loss_sum*1.0/count
+
+    #Save accuracy and loss to lists
+    epoch_accuracy_normal.append(normal_accuracy)
+    epoch_accuracy_average.append(average_accuracy)
+    epoch_loss.append(loss_sum)
+    
+    print(f'{mode} {epoch}| Loss: {loss_sum}| {mode} acc: {normal_accuracy}| {mode} avg acc: {average_accuracy}')
+
+    #The model with the highest found accuracy for testing is saved
+    if mode == 'Testing':
+        if normal_accuracy > best_test_accuracy:
+            best_test_accuracy = normal_accuracy 
+            print('[INFO] Saving Model...')
+            np.savetxt('Epoch.txt',np.array([epoch]))
+            torch.save(model.state_dict(),'model.t7')
+            return best_test_accuracy
+
+
+def save_to_csvs(mode, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
+    np.savetxt(f'{mode}_Accuracy_normal.csv',np.array(epoch_accuracy_normal))
+    np.savetxt(f'{mode}_Accuracy_average.csv',np.array(epoch_accuracy_average))
+    np.savetxt(f'{mode}_Loss.csv',np.array(epoch_loss))
+
+
+def new_main():
+    model = DGCNN(numClass = NUM_CLASS, emb_dims = EMB_DIMS, dropout_rate=DROPOUT_RATE, batch_size = TRAIN_BATCH_SIZE, k = K).to(device)
+
+    device = torch.device("cuda")
+    print("Cuda: ",torch.cuda.get_device_name())
+
+    #Loading the dataloaders for train and test
+    train_loader = DataLoader(ModelNet40(partition='train', num_points=NUM_POINTS), num_workers=8,
+                              batch_size=TRAIN_BATCH_SIZE, shuffle=True, drop_last=True)
+    test_loader = DataLoader(ModelNet40(partition='test', num_points=NUM_POINTS), num_workers=8,
+                             batch_size=TEST_BATCH_SIZE, shuffle=True, drop_last=False)
+
+    #Optimizer
+    optimizer = optim.SGD(model.parameters(),lr = LR*100 , momentum = 0.9, weight_decay = 1e-4)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, EPOCHS, eta_min= LR)
+
+    #Loss function
+    criterion = cal_loss #FOR US: Maybe just overwrite this with a function instead of calling into utils, as we just use one line from the other script. 
+
+    #Lists to save information such as accuracy and loss for each epoch.
+    best_test_accuracy = 0
+    
+    train_epoch_accuracy_normal = []
+    train_epoch_accuracy_average = []
+    train_epoch_loss = []
+
+    test_epoch_accuracy_normal = []
+    test_epoch_accuracy_average = []
+    test_epoch_loss = []
+
+    for epoch in range(EPOCHS):
+        train1(model, 'Training', train_loader, train_epoch_accuracy_normal, train_epoch_accuracy_average, train_epoch_loss)
+        best_test_accuracy = train1(model, 'Testing', test_loader, test_epoch_accuracy_normal, test_epoch_accuracy_average, test_epoch_loss)
+
+    #Saves the accuracy and lost lists into csvs
+    save_to_csvs(train_epoch_accuracy_normal, train_epoch_accuracy_average, train_epoch_loss)
+    save_to_csvs(test_epoch_accuracy_normal, test_epoch_accuracy_average, test_epoch_loss)
+
 if __name__ == "__main__":
+    #new_main():
     train()
     test()
     
---
+++ ComputerVision_miniproject/readme.txt
@@ -0,0 +1,10 @@
+This implementation is done by group 843, for the course Image Processing and Computer Vision 2020.
+ It is an implementation based om the Dynamic Graph CNN, inspired by https://github.com/WangYueFt/dgcnn
+
+Before running you need to download the data from http://modelnet.cs.princeton.edu/ModelNet40.zip
+
+To run the training, run the main.py. All the parameters we used, is already set in the begining of the script
+
+In the folder 'Model and Results' files with the model, accuracy and loss can be found. Here there is also a script 'graph.py' which is used 
+to generate the graphs used in the report.
+
