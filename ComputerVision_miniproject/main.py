071f7c98eef524b02838d6c2e160a05ab361d4c8
Lynzzen <mlynge16@student.aau.dk>
2020-04-24 18:22:34 +0200

    New update

--- ComputerVision_miniproject/main.py
+++ ComputerVision_miniproject/main.py
@@ -20,8 +20,8 @@ NUM_CLASS = 40
 EMB_DIMS = 1024
 NUM_POINTS = 1024
 DROPOUT_RATE = 0.5
-TRAIN_BATCH_SIZE = 16
-TEST_BATCH_SIZE = 8
+TRAIN_BATCH_SIZE = 1
+TEST_BATCH_SIZE = 1
 EPOCHS = 50
 K = 20
 LR = 0.001
@@ -197,118 +197,113 @@ def test():
 
 
 
+# def train1(model, mode, dataloader, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
+#     print('[INFO] {mode}...')
+#     #Selecting which mode to run the model (Training or Testing)
+#     if mode == 'Training':        
+#         scheduler.step()
+#         model.train()
+#     else:
+#         model.eval()
 
-
-
-
-
-def train1(model, mode, dataloader, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
-    print('[INFO] {mode}...')
-    #Selecting which mode to run the model (Training or Testing)
-    if mode == 'Training':        
-        scheduler.step()
-        model.train()
-    else:
-        model.eval()
-
-    loss_sum = 0.0
-    count = 0.0
-    predicted_labels = []
-    correct_labels = []
+#     loss_sum = 0.0
+#     count = 0.0
+#     predicted_labels = []
+#     correct_labels = []
     
-    for data, label in tqdm(dataloader):
-        data = data.to(device) 
-        label = label.to(device).squeeze()
+#     for data, label in tqdm(dataloader):
+#         data = data.to(device) 
+#         label = label.to(device).squeeze()
 
-        data = data.permute(0,2,1)
-        batch_size = data.size()[0]
+#         data = data.permute(0,2,1)
+#         batch_size = data.size()[0]
         
-        if mode == 'Training':
-            optimizer.zero_grad() #zero_grad clears old gradients from the last step (otherwise you’d just accumulate the gradients from all loss.backward() calls).
+#         if mode == 'Training':
+#             optimizer.zero_grad() #zero_grad clears old gradients from the last step (otherwise you’d just accumulate the gradients from all loss.backward() calls).
 
-        output = model(data) #Calculates class probabilities for each point cloud. So if batchsize is 16 and num classes is 40, it returns [16,40].
-        loss = criterion(output,label,False) 
+#         output = model(data) #Calculates class probabilities for each point cloud. So if batchsize is 16 and num classes is 40, it returns [16,40].
+#         loss = criterion(output,label,False) 
 
-        if mode == 'Training':
-            loss.backward() #computes the derivative of the loss w.r.t. the parameters (or anything requiring gradients) using backpropagation.
-            optimizer.step() #causes the optimizer to take a step based on the gradients of the parameters.
+#         if mode == 'Training':
+#             loss.backward() #computes the derivative of the loss w.r.t. the parameters (or anything requiring gradients) using backpropagation.
+#             optimizer.step() #causes the optimizer to take a step based on the gradients of the parameters.
 
-        predictions = output.max(dim=1)[1]
-        count += batch_size
+#         predictions = output.max(dim=1)[1]
+#         count += batch_size
 
-        loss_sum += loss.item()*batch_size
-        correct_labels.append(label.cpu().numpy())
-        predicted_labels.append(predictions.detach().cpu().numpy()) 
+#         loss_sum += loss.item()*batch_size
+#         correct_labels.append(label.cpu().numpy())
+#         predicted_labels.append(predictions.detach().cpu().numpy()) 
 
-    #Concats 
-    predicted_labels = np.concatenate(predicted_labels)
-    correct_labels = np.concatenate(correct_labels)
+#     #Concats 
+#     predicted_labels = np.concatenate(predicted_labels)
+#     correct_labels = np.concatenate(correct_labels)
 
-    #Computes accuracy and loss based on metrics
-    normal_accuracy = metrics.accuracy_score(correct_labels,predicted_labels)
-    average_accuracy = metrics.balanced_accuracy_score(correct_labels,predicted_labels)
-    loss_sum = loss_sum*1.0/count
+#     #Computes accuracy and loss based on metrics
+#     normal_accuracy = metrics.accuracy_score(correct_labels,predicted_labels)
+#     average_accuracy = metrics.balanced_accuracy_score(correct_labels,predicted_labels)
+#     loss_sum = loss_sum*1.0/count
 
-    #Save accuracy and loss to lists
-    epoch_accuracy_normal.append(normal_accuracy)
-    epoch_accuracy_average.append(average_accuracy)
-    epoch_loss.append(loss_sum)
+#     #Save accuracy and loss to lists
+#     epoch_accuracy_normal.append(normal_accuracy)
+#     epoch_accuracy_average.append(average_accuracy)
+#     epoch_loss.append(loss_sum)
     
-    print(f'{mode} {epoch}| Loss: {loss_sum}| {mode} acc: {normal_accuracy}| {mode} avg acc: {average_accuracy}')
+#     print(f'{mode} {epoch}| Loss: {loss_sum}| {mode} acc: {normal_accuracy}| {mode} avg acc: {average_accuracy}')
 
-    #The model with the highest found accuracy for testing is saved
-    if mode == 'Testing':
-        if normal_accuracy > best_test_accuracy:
-            best_test_accuracy = normal_accuracy 
-            print('[INFO] Saving Model...')
-            np.savetxt('Epoch.txt',np.array([epoch]))
-            torch.save(model.state_dict(),'model.t7')
-            return best_test_accuracy
-
-
-def save_to_csvs(mode, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
-    np.savetxt(f'{mode}_Accuracy_normal.csv',np.array(epoch_accuracy_normal))
-    np.savetxt(f'{mode}_Accuracy_average.csv',np.array(epoch_accuracy_average))
-    np.savetxt(f'{mode}_Loss.csv',np.array(epoch_loss))
+#     #The model with the highest found accuracy for testing is saved
+#     if mode == 'Testing':
+#         if normal_accuracy > best_test_accuracy:
+#             best_test_accuracy = normal_accuracy 
+#             print('[INFO] Saving Model...')
+#             np.savetxt('Epoch.txt',np.array([epoch]))
+#             torch.save(model.state_dict(),'model.t7')
+#             return best_test_accuracy
 
 
-def new_main():
-    model = DGCNN(numClass = NUM_CLASS, emb_dims = EMB_DIMS, dropout_rate=DROPOUT_RATE, batch_size = TRAIN_BATCH_SIZE, k = K).to(device)
+# def save_to_csvs(mode, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
+#     np.savetxt(f'{mode}_Accuracy_normal.csv',np.array(epoch_accuracy_normal))
+#     np.savetxt(f'{mode}_Accuracy_average.csv',np.array(epoch_accuracy_average))
+#     np.savetxt(f'{mode}_Loss.csv',np.array(epoch_loss))
 
-    device = torch.device("cuda")
-    print("Cuda: ",torch.cuda.get_device_name())
 
-    #Loading the dataloaders for train and test
-    train_loader = DataLoader(ModelNet40(partition='train', num_points=NUM_POINTS), num_workers=8,
-                              batch_size=TRAIN_BATCH_SIZE, shuffle=True, drop_last=True)
-    test_loader = DataLoader(ModelNet40(partition='test', num_points=NUM_POINTS), num_workers=8,
-                             batch_size=TEST_BATCH_SIZE, shuffle=True, drop_last=False)
+# def new_main():
+#     model = DGCNN(numClass = NUM_CLASS, emb_dims = EMB_DIMS, dropout_rate=DROPOUT_RATE, batch_size = TRAIN_BATCH_SIZE, k = K).to(device)
 
-    #Optimizer
-    optimizer = optim.SGD(model.parameters(),lr = LR*100 , momentum = 0.9, weight_decay = 1e-4)
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, EPOCHS, eta_min= LR)
+#     device = torch.device("cuda")
+#     print("Cuda: ",torch.cuda.get_device_name())
 
-    #Loss function
-    criterion = cal_loss #FOR US: Maybe just overwrite this with a function instead of calling into utils, as we just use one line from the other script. 
+#     #Loading the dataloaders for train and test
+#     train_loader = DataLoader(ModelNet40(partition='train', num_points=NUM_POINTS), num_workers=8,
+#                               batch_size=TRAIN_BATCH_SIZE, shuffle=True, drop_last=True)
+#     test_loader = DataLoader(ModelNet40(partition='test', num_points=NUM_POINTS), num_workers=8,
+#                              batch_size=TEST_BATCH_SIZE, shuffle=True, drop_last=False)
 
-    #Lists to save information such as accuracy and loss for each epoch.
-    best_test_accuracy = 0
+#     #Optimizer
+#     optimizer = optim.SGD(model.parameters(),lr = LR*100 , momentum = 0.9, weight_decay = 1e-4)
+#     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, EPOCHS, eta_min= LR)
+
+#     #Loss function
+#     criterion = cal_loss #FOR US: Maybe just overwrite this with a function instead of calling into utils, as we just use one line from the other script. 
+
+#     #Lists to save information such as accuracy and loss for each epoch.
+#     best_test_accuracy = 0
     
-    train_epoch_accuracy_normal = []
-    train_epoch_accuracy_average = []
-    train_epoch_loss = []
+#     train_epoch_accuracy_normal = []
+#     train_epoch_accuracy_average = []
+#     train_epoch_loss = []
 
-    test_epoch_accuracy_normal = []
-    test_epoch_accuracy_average = []
-    test_epoch_loss = []
+#     test_epoch_accuracy_normal = []
+#     test_epoch_accuracy_average = []
+#     test_epoch_loss = []
 
-    for epoch in range(EPOCHS):
-        train1(model, 'Training', train_loader, train_epoch_accuracy_normal, train_epoch_accuracy_average, train_epoch_loss)
-        best_test_accuracy = train1(model, 'Testing', test_loader, test_epoch_accuracy_normal, test_epoch_accuracy_average, test_epoch_loss)
+#     for epoch in range(EPOCHS):
+#         train1(model, 'Training', train_loader, train_epoch_accuracy_normal, train_epoch_accuracy_average, train_epoch_loss)
+#         best_test_accuracy = train1(model, 'Testing', test_loader, test_epoch_accuracy_normal, test_epoch_accuracy_average, test_epoch_loss)
 
-    #Saves the accuracy and lost lists into csvs
-    save_to_csvs(train_epoch_accuracy_normal, train_epoch_accuracy_average, train_epoch_loss)
-    save_to_csvs(test_epoch_accuracy_normal, test_epoch_accuracy_average, test_epoch_loss)
+#     #Saves the accuracy and lost lists into csvs
+#     save_to_csvs(train_epoch_accuracy_normal, train_epoch_accuracy_average, train_epoch_loss)
+#     save_to_csvs(test_epoch_accuracy_normal, test_epoch_accuracy_average, test_epoch_loss)
 
 if __name__ == "__main__":
     #new_main():
