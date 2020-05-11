47cf9b932c96dd89ee6b04008e674a04d15d3294
Moaaz <mallah16@student.aau.dk>
2020-05-11 20:53:06 +0200

    Adding the random parameters

--- Main project/PointCloud_Classification_DGCNN_Group843/Code/main.py
+++ Main project/PointCloud_Classification_DGCNN_Group843/Code/main.py
@@ -8,35 +8,54 @@ from data import PointCloudDataset
 import torch.nn.functional as F
 import sklearn.metrics as metrics
 from tqdm import tqdm
+import os
 
 #Static Parameters
 NUM_CLASS = 2
 EMB_DIMS = 1024
 NUM_POINTS = 1024
-DROPOUT_RATE = 0.5
-TRAIN_BATCH_SIZE = 16
-TEST_BATCH_SIZE = 8
-EPOCHS = 20
-K = 20 #20
-LR = 0.00000001
-
-device = torch.device("cuda")
-
-model = DGCNN(numClass = NUM_CLASS, emb_dims = EMB_DIMS, dropout_rate=DROPOUT_RATE, batch_size = TRAIN_BATCH_SIZE, k = K).to(device)
+EPOCHS = 2
 
 #def count_parameters(model):
 #    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 #print(count_parameters(model))
 
+def random_hyperparamter_search(current_path, dropout, train_batchsize, test_batchsize, k, lr, momentum, weight_decay):
+    dropout = np.random.uniform(dropout[0],dropout[1])
+    train_batchsize = np.random.randint(train_batchsize[0],train_batchsize[1])
+    test_batchsize = np.random.randint(test_batchsize[0],test_batchsize[1])
+    k = np.random.randint(k[0],k[1])
+
+    lr_list = list(np.logspace(np.log10(lr[0]), np.log10(lr[1]), base = 10, num = 1000))
+    lr = lr_list[np.random.randint(0,len(lr_list))]
+
+    momentum = np.random.uniform(momentum[0],momentum[1])
+    weight_decay = np.random.uniform(weight_decay[0],weight_decay[1])
+
+    hyperparameters = {"dropout_rate" : dropout,
+                        "train_batchsize" : train_batchsize,
+                        "test_batchsize": test_batchsize,
+                        "k":k,
+                        "lr":lr,
+                        "momentum":momentum,
+                        "weight_decay":weight_decay}
+
+    with open(f'{current_path}/hyperparameters.txt', 'w') as f:
+        for key in hyperparameters:
+            f.write(f"{key}: {hyperparameters[key]}\n")
+
+    return dropout, train_batchsize, test_batchsize, k, lr, momentum, weight_decay
+
 
 def cal_loss(pred, label):
     label = label.contiguous().view(-1) #This One
     loss = F.cross_entropy(pred, label, reduction='mean') #BCELoss 
-    #loss = nn.BCELoss(pred, label, reduction='mean') #BCELoss 
+    #nn.BCEWithLogitsLoss
+    #loss = nn.BCEWithLogitsLoss(pred, label, reduction='mean') #BCELoss 
     return loss
 
-def train(model, epoch, mode, dataloader, optimizer, scheduler, criterion, best_test_accuracy, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
+def train(current_path, model, epoch, mode, dataloader, optimizer, scheduler, criterion, best_test_accuracy, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
     print(f'[INFO] {mode}...')
     #Selecting which mode to run the model (Training or Testing)
     if mode == 'Training':
@@ -91,16 +110,15 @@ def train(model, epoch, mode, dataloader, optimizer, scheduler, criterion, best_
     print(f'{mode} {epoch}| Loss: {loss_sum}| {mode} acc: {normal_accuracy}| {mode} avg acc: {average_accuracy}')
 
     #The model with the highest found accuracy for testing is saved
-    if mode == 'TestingXXX':
+    if mode == 'Validation':
         if normal_accuracy > best_test_accuracy:
             best_test_accuracy = normal_accuracy
             print('[INFO] Saving Model...')
-            np.savetxt('Epoch.txt',np.array([epoch]))
-            torch.save(model.state_dict(),'model.t7')
+            np.savetxt(f'{current_path}/Epoch.txt', [epoch])
+            torch.save(model,f'{current_path}/model.t7')
             return best_test_accuracy
-
 def test(model):
-    test_loader = DataLoader(PointCloudDataset(partition='Testing', num_points=NUM_POINTS), batch_size=TEST_BATCH_SIZE, shuffle=True, drop_last=False)
+    test_loader = DataLoader(PointCloudDataset(partition='Testing', num_points=NUM_POINTS), batch_size=TEST_BATCHSIZE, shuffle=True, drop_last=False)
 
     model = model.eval()
 
@@ -124,31 +142,41 @@ def test(model):
     test_acc = metrics.accuracy_score(correct_labels, predicted_labels)
     print(f'Test Accuracy = {test_acc}')
 
-def save_to_csvs(mode, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
-    np.savetxt(f'{mode}_Accuracy_normal.csv',np.array(epoch_accuracy_normal))
-    np.savetxt(f'{mode}_Accuracy_average.csv',np.array(epoch_accuracy_average))
-    np.savetxt(f'{mode}_Loss.csv',np.array(epoch_loss))
+def save_to_csvs(current_path, mode, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
+    np.savetxt(f'{current_path}/{mode}_Accuracy_normal.csv',np.array(epoch_accuracy_normal))
+    np.savetxt(f'{current_path}/{mode}_Accuracy_average.csv',np.array(epoch_accuracy_average))
+    np.savetxt(f'{current_path}/{mode}_Loss.csv',np.array(epoch_loss))
 
 if __name__ == "__main__":
+    num_subfolders_in_iterations = len(os.listdir("Iterations"))+1
+    os.mkdir(f"Iterations/Iteration_{num_subfolders_in_iterations}")
+    current_path = f"Iterations/Iteration_{num_subfolders_in_iterations}"
+
+    device = torch.device("cuda")
     print("Cuda: ",torch.cuda.get_device_name())
 
-    #Loading the dataloaders for train and test
-    #train_loader = DataLoader(ModelNet40(partition='train', num_points=NUM_POINTS), num_workers=8,
-    #                          batch_size=TRAIN_BATCH_SIZE, shuffle=True, drop_last=True)
-    #test_loader = DataLoader(ModelNet40(partition='test', num_points=NUM_POINTS), num_workers=8,
-    #                         batch_size=TEST_BATCH_SIZE, shuffle=True, drop_last=False)
+    print("running main ln157")
+
+    DROPOUT_RATE, TRAIN_BATCHSIZE, TEST_BATCHSIZE, K, LR = random_hyperparamter_search(current_path, dropout = [0.5,0.8], #Normal 0.5
+                                                                            train_batchsize = [16,17], #Normal 16: 16 only max exclusive
+                                                                            test_batchsize = [8,9], #Normal 8: 8 only, max exclusive
+                                                                            k = [15,26], #Normal 20: 15 to 25
+                                                                            lr = [0.000001,0.001], #Normal 0.000001: 
+                                                                            momentum = [0.9,0.99], #Normal: 0.9
+                                                                            weight_decay = [1e-4,1e-5]) #Normal: 1e-4
+
+    model = DGCNN(numClass = NUM_CLASS, emb_dims = EMB_DIMS, dropout_rate=DROPOUT_RATE, batch_size = TRAIN_BATCHSIZE, k = K).to(device)
+
     train_loader = DataLoader(PointCloudDataset(partition='Training', num_points=NUM_POINTS), num_workers=8,
-                              batch_size=TRAIN_BATCH_SIZE, shuffle=True, drop_last=True)
+                            batch_size=TRAIN_BATCHSIZE, shuffle=True, drop_last=True)
     test_loader = DataLoader(PointCloudDataset(partition='Validation', num_points=NUM_POINTS), num_workers=8,
-                             batch_size=TEST_BATCH_SIZE, shuffle=True, drop_last=False)
+                            batch_size=TRAIN_BATCHSIZE, shuffle=True, drop_last=False)
 
     #Optimizer
-    optimizer = optim.SGD(model.parameters(),lr = LR*100 , momentum = 0.8, weight_decay = 1e-4) #0.9 normal momentum
+    optimizer = optim.SGD(model.parameters(),lr = LR*100 , momentum = 0.9, weight_decay = 1e-4) #0.9 normal momentum, LR*100
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, EPOCHS, eta_min= LR)
     
-
     #Loss function
-    #criterion = cal_loss
     criterion = cal_loss
 
     #Lists to save information such as accuracy and loss for each epoch.
@@ -163,20 +191,22 @@ if __name__ == "__main__":
     test_epoch_loss = []
 
     #Go through all epochs
-    for epoch in range(EPOCHS):
-        train(model, epoch, 'Training', train_loader, optimizer, scheduler, criterion, best_test_accuracy,
-                                                                                       train_epoch_accuracy_normal,
-                                                                                       train_epoch_accuracy_average,
-                                                                                       train_epoch_loss)
 
-        best_test_accuracy = train(model, epoch, 'Validation', test_loader, optimizer, scheduler, criterion, best_test_accuracy,
-                                                                                                          test_epoch_accuracy_normal,
-                                                                                                          test_epoch_accuracy_average,
-                                                                                                          test_epoch_loss)
+    for epoch in range(EPOCHS):
+        print("running train ln186")
+        train(current_path, model, epoch, 'Training', train_loader, optimizer, scheduler, criterion, best_test_accuracy,
+                                                                                    train_epoch_accuracy_normal,
+                                                                                    train_epoch_accuracy_average,
+                                                                                    train_epoch_loss)
+
+        best_test_accuracy = train(current_path, model, epoch, 'Validation', test_loader, optimizer, scheduler, criterion, best_test_accuracy,
+                                                                                                        test_epoch_accuracy_normal,
+                                                                                                        test_epoch_accuracy_average,
+                                                                                                        test_epoch_loss)
 
         #Saves the accuracy and lost lists into csvs save_to_csvs(mode, epoch_accuracy_normal, epoch_accuracy_average, epoch_loss):
-        save_to_csvs('Train',train_epoch_accuracy_normal, train_epoch_accuracy_average, train_epoch_loss)
-        save_to_csvs('Test',test_epoch_accuracy_normal, test_epoch_accuracy_average, test_epoch_loss)
+        save_to_csvs(current_path, 'Train',train_epoch_accuracy_normal, train_epoch_accuracy_average, train_epoch_loss)
+        save_to_csvs(current_path, 'Test',test_epoch_accuracy_normal, test_epoch_accuracy_average, test_epoch_loss)
 
     #Testing
     test(model)
