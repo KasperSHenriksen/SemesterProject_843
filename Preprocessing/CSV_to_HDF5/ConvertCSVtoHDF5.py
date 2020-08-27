9656b7609fa2ea943e186164a6f5122ad2049192
Schonsteren <schonsteren@gmail.com>
2020-08-27 19:45:39 +0200

    Updated preprocessing.

--- Preprocessing/CSV_to_HDF5/ConvertCSVtoHDF5.py
+++ Preprocessing/CSV_to_HDF5/ConvertCSVtoHDF5.py
@@ -3,44 +3,48 @@ import os
 from tqdm.auto import tqdm
 import numpy as np
 import random
+import glob
 
-def CreateDatasetHDF5(pathdir,pointcloud_size):
+
+def get_filepaths(pathdir):
+    filepaths = glob.glob(f'{pathdir}/**/*.csv',recursive=True)
+    filepaths.extend(glob.glob(f'{pathdir}/**/*.CSV',recursive=True))
+    return filepaths
+
+def create_dataset_HDF5(pathdir,pointcloud_size):
     with h5py.File(f'{pathdir}/pointcloud_hdf5.h5','w') as hdf:
         defect_group = hdf.create_group('Defect')
         fine_group = hdf.create_group('Fine')
 
         groups = [defect_group,fine_group]
-        folders = os.listdir(f'{pathdir}')
+        class_folders = os.listdir(f'{pathdir}')
 
-        zipping = zip(groups,folders)
+        zipping = zip(groups,class_folders)
 
         print(f'Converting cvs to hdf5 as: "pointcloud_hdf5.h5".')
-        for group, folder in zipping:
-            print(f'Converting {folder} cvs...')
-            for idx,pointcloud in enumerate(tqdm(os.listdir(f'{pathdir}/{folder}'))):
+        for group, class_folders in zipping:
+            print(f'Converting {class_folders} cvs...')
+            filepaths = get_filepaths(f'{pathdir}/{class_folders}')
 
-                array = np.genfromtxt(f'{pathdir}/{folder}/{pointcloud}',delimiter=',', dtype = np.float32)
-                #array = np.delete(array,slice(pointcloud_size,len(array)),0)
+            for idx, fp in enumerate(tqdm(filepaths)):
+                array = np.genfromtxt(fp, delimiter=',', dtype = np.float32)
                 group.create_dataset(f'pc{idx}',data=array)
 
 def shuffle(pathdir):
     with h5py.File(f'{pathdir}/pointcloud_hdf5.h5','r') as hdf:
         combined_pointcloud_list = []
         combined_label_list = []
-        print(hdf.keys())
         for index, group in enumerate(list(hdf.keys())):
             pointcloud_list = list(hdf.get(f'{group}'))
-
             for pointcloud in pointcloud_list:
                 pointcloud = hdf.get(f'{group}/{pointcloud}')
+
                 pointcloud = np.array(pointcloud)
 
                 combined_pointcloud_list.append(pointcloud)
                 #combined_label_list.append(np.eye(2,dtype=np.int64)[index]) #Hot encoding
                 label = np.array(index,dtype=np.int64)
                 combined_label_list.append(np.array(label))
-                print(f"Group: {group} and index: {index}")
-                break
 
         temp_zip = list(zip(combined_pointcloud_list,combined_label_list))
         random.shuffle(temp_zip)
@@ -52,6 +56,7 @@ def combine_and_shuffle(pathdir,training,validation_ratio = 0.15):
     pointclouds, labels = shuffle(pathdir)
     with h5py.File(f'{pathdir}/pointcloud_hdf5.h5','w') as hdf:
         if(training == True):
+            print(f'point clouds: {len(pointclouds)}. validation ratio: {validation_ratio}')
             size = int(len(pointclouds)*validation_ratio)
 
             #Validation Set
@@ -80,12 +85,12 @@ path = input("Enter path to dataset: ")
 training_or_testing = input("Training or Testing: ")
 
 if(training_or_testing == 'Training'):
-    validation_ratio = float(input("Enter the validation ratio for splitting the dataset into training and validation: "))
+    validation_ratio = float(input("Enter the validation ratio for splitting (0.0-1.0): "))
 
-    CreateDatasetHDF5(path,1024)
+    create_dataset_HDF5(path,1024)
     combine_and_shuffle(path,training = True,validation_ratio = validation_ratio)
 else:
-    CreateDatasetHDF5(path,1024)
+    create_dataset_HDF5(path,1024)
     combine_and_shuffle(path,training = False,validation_ratio = 0)
 
 
--- Preprocessing/SubSampling/pointcloud_subsampling_py.py
+++ Preprocessing/SubSampling/pointcloud_subsampling_py.py
@@ -4,28 +4,52 @@ import numpy as np
 import csv
 from tqdm import tqdm
 import time
+import glob
 
 def get_filepaths(main_folder):
-    folders = set(os.listdir(f'{main_folder}'))
-    non_subsampled_filepaths =[]
-    for folder in folders:
-        files = os.listdir(rf'{main_folder}/{folder}')
-        for file in files:
-            non_subsampled_filepaths.append(rf'{main_folder}/{folder}/{file}')
-
-    return non_subsampled_filepaths
+    files = glob.glob(f'{main_folder}/**/*.csv',recursive=True)
+    files.extend(glob.glob(f'{main_folder}/**/*.CSV',recursive=True))
+    return files
 
 def subsampling_algorithm(main_folder,algorithm,parameter_value):
+    os_command = ''
+    if os.name == 'nt':
+        os_command = 'CloudCompare'
+    elif os.name == 'posix':
+        os_command = 'cloudcompare.CloudCompare'
+
     non_subsampled_filepaths = get_filepaths(main_folder)
-
-
     for filepath in non_subsampled_filepaths:
-        os.system(f'CloudCompare -SILENT -O {filepath} -C_EXPORT_FMT ASC -EXT CSV -PREC 6 -SEP COMMA -SS {algorithm} {parameter_value}')
-        #os.system(f'cloudcompare.CloudCompare -SILENT -O {filepath} -C_EXPORT_FMT ASC -EXT CSV -PREC 6 -SEP COMMA -SS {algorithm} {parameter_value}')
+        print(filepath)
+        os.system(f'{os_command} -SILENT -O {filepath} -C_EXPORT_FMT ASC -EXT CSV -PREC 6 -SEP COMMA -SS {algorithm} {parameter_value}')
         os.remove(filepath)
 
+def delete_pointclouds_below_limit(main_folder,limit):
+    if (input_function('Delete pointclouds containing less than 1024 points? (y/n): ') == False):
+        return
+
+    print('[INFO] Deleting Pointclouds below limit...')
+    filepaths = get_filepaths(main_folder)
+    for file in tqdm(filepaths):
+        old_f = pd.read_csv(f'{file}')
+        if len(old_f)+1 < limit:
+            os.remove(file)
+
+def delete_defective_pcs_without_defective_points(main_folder):
+    if (input_function('Delete pointclouds containing no defected points? (y/n): ') == False):
+        return
+    
+    print('[INFO] Deleting Defective Pointclouds that have lost all their defective points...')
+    filepaths = get_filepaths(main_folder+'/Defective')
+    for file in tqdm(filepaths):
+        csv = pd.read_csv(f'{file}')
+        if any(csv.iloc[:, 3] == 1) != True:
+            os.remove(file)
 
 def remove_label_column(main_folder):
+    if (input_function('Remove label for each point in point clouds? (y/n): ') == False):
+        return
+
     print('[INFO] Remove label column...')
     filepaths = get_filepaths(main_folder)
 
@@ -36,33 +60,27 @@ def remove_label_column(main_folder):
             new_f.to_csv(f'{file}',index=False)
         except:
             print(file)
+    
+def input_function(message):
+    answer = input(message).lower()
+    while(True):
+        if answer == 'y':
+            return True
+        elif answer == 'n':
+            return False
+        else:
+            answer = input(message).lower()
 
-def delete_pointclouds_below_limit(main_folder,limit):
-    print('[INFO] Deleting Pointclouds below limit...')
-    filepaths = get_filepaths(main_folder)
-
-    for file in tqdm(filepaths):
-        old_f = pd.read_csv(f'{file}')
-        if len(old_f)+1 < limit:
-            os.remove(file)
 
 main_folder = input('Path to data: ')
-#main_folder = '/home/kasper/Desktop/Test_data/old_unity2'
 cloud_compare_folder = input('Path to CloudCompare: ')
 os.chdir(cloud_compare_folder)
-#os.chdir('/snap/bin')
 
-remove_label_column(main_folder)
+subsampling_algorithm(main_folder,'SPATIAL',0.03)
 
-start1 = time.time()
-subsampling_algorithm(main_folder,'SPATIAL',0.08)
-stop1 = time.time()
-
-start2 = time.time()
 subsampling_algorithm(main_folder,'RANDOM',1024)
-stop2 = time.time()
 
-print('Space:',stop1-start1)
-print('Random:',stop2-start2)
-#remove_label_column(main_folder)
 delete_pointclouds_below_limit(main_folder,1024)
+
+delete_defective_pcs_without_defective_points(main_folder)
+remove_label_column(main_folder)
